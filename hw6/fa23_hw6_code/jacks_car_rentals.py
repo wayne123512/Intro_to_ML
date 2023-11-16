06eecc592ede6e085c7f34ef81d1641720e03ba6
wayne123512 <yuwen_chen@berkeley.edu>
2023-11-16 00:07:28 -0800

    final codes

--- hw6/fa23_hw6_code/jacks_car_rentals.py
+++ hw6/fa23_hw6_code/jacks_car_rentals.py
@@ -29,7 +29,7 @@ def cdf(k: int, lam: float):
 def expectation(lam: float, end: int):
     """
     Suppose X ~ Poisson(lam).
-    This function computes E[max(X, end)].
+    This function computes E[min(X, end)].
     """
     return sum(k * pmf(k, lam) for k in range(end + 1)) + (1 - cdf(end, lam)) * end
 
@@ -61,27 +61,27 @@ def transition_prob(state: tuple[int], action: int, next_state: tuple[int]) -> f
     # Case 1: we can only rent up to `loc_1`` cars the next day
     # To reach `loc_1_new`` cars by the end of the day, how many cars do we need to 
     # rent out at the bare minimum?
-    min_cars_to_rent = ... # TODO
+    min_cars_to_rent = max(loc_1 - loc_1_new, 0) # TODO
     for rented in range(min_cars_to_rent, loc_1 + 1):
         # Now, find out the number of cars we want to see returned to reach `loc_1_new`, 
         # given that we rented out `rented` cars:
-        returned = ... # TODO
+        returned = loc_1_new - loc_1 + rented # TODO
         # We again have two cases:
         if loc_1_new < MAX_CARS_LOC_1:
             # Case 1.1: loc_1_new < MAX_CARS_LOC_1
             # Here, we need exactly `returned` cars to be returned. 
             # Compute the probability of seeing exactly `returned` cars returned at location 1.
-            prob_returned = ... # TODO
+            prob_returned = pmf(returned, LOC_1_RETURN_LAMBDA) # TODO
         else:
             # Case 1.2: loc_1_new == MAX_CARS_LOC_1
             # Here, we need at least `returned` cars to be returned.
             # The excess cars are removed from the system, as mentioned in the problem.
             # Compute the probability of seeing at least `returned` cars returned at location 1.
-            prob_returned = ... # TODO
+            prob_returned = sf(returned-1, LOC_1_RETURN_LAMBDA) # TODO
         # Compute the probability of seeing exactly `rented` cars being rented out.
-        prob_rented = ... # TODO
+        prob_rented = pmf(rented, LOC_1_RENTAL_LAMBDA) # TODO
         # Combine the two probabilities above. Note that the rentals and returns are independent.
-        prob_loc_1 += ... # TODO
+        prob_loc_1 += prob_returned * prob_rented # TODO
     # Case 2: we get more rental requests than we have cars available at location 1.
     # Of course, we can only rent out `loc_1`` cars before running out but that doesn't affect
     # the number of requests that come in. 
@@ -89,7 +89,7 @@ def transition_prob(state: tuple[int], action: int, next_state: tuple[int]) -> f
     # `loc_1_new`` cars to be returned. 
     # Compute the probability of this event (more than `loc_1` rental requests and exactly `loc_1_new`
     # returns) happening.
-    prob_loc_1 += ... # TODO
+    prob_loc_1 += pmf(loc_1_new, LOC_1_RETURN_LAMBDA) * sf(loc_1, LOC_1_RENTAL_LAMBDA) # TODO
 
     # We repeat the same process for location 2
     prob_loc_2 = 0
@@ -98,27 +98,27 @@ def transition_prob(state: tuple[int], action: int, next_state: tuple[int]) -> f
     # Case 1: we can only rent up to `loc_2`` cars the next day
     # To reach `loc_2_new`` cars by the end of the day, how many cars do we need to 
     # rent out at the bare minimum?
-    min_cars_to_rent = ... # TODO
+    min_cars_to_rent = max(loc_2 - loc_2_new, 0) # TODO
     for rented in range(min_cars_to_rent, loc_2 + 1):
         # Now, find out the number of cars we want to see returned to reach `loc_2_new`, 
         # given that we rented out `rented` cars:
-        returned = ... # TODO
+        returned = loc_2_new - loc_2 + rented  # TODO
         # We again have two cases:
         if loc_2_new < MAX_CARS_LOC_2:
             # Case 1.1: loc_2_new < MAX_CARS_LOC_2
             # Here, we need exactly `returned` cars to be returned. 
             # Compute the probability of seeing exactly `returned` cars returned at location 2.
-            prob_returned = ... # TODO
+            prob_returned = pmf(returned, LOC_2_RETURN_LAMBDA)  # TODO
         else:
             # Case 1.2: loc_2_new == MAX_CARS_LOC_2
             # Here, we need at least `returned` cars to be returned.
             # The excess cars are removed from the system, as mentioned in the problem.
             # Compute the probability of seeing at least `returned` cars returned at location 2.
-            prob_returned = ... # TODO
+            prob_returned = sf(returned-1, LOC_2_RETURN_LAMBDA)  # TODO
         # Compute the probability of seeing exactly `rented` cars being rented out.
-        prob_rented = ... # TODO
+        prob_rented = pmf(rented, LOC_2_RENTAL_LAMBDA) # TODO
         # Combine the two probabilities above. Note that the rentals and returns are independent.
-        prob_loc_2 += ... # TODO
+        prob_loc_2 += prob_returned * prob_rented # TODO
     # Case 2: we get more rental requests than we have cars available at location 2.
     # Of course, we can only rent out `loc_2`` cars before running out but that doesn't affect
     # the number of requests that come in. 
@@ -126,7 +126,7 @@ def transition_prob(state: tuple[int], action: int, next_state: tuple[int]) -> f
     # `loc_2_new`` cars to be returned. 
     # Compute the probability of this event (more than `loc_2` rental requests and exactly `loc_2_new`
     # returns) happening.
-    prob_loc_2 += ... # TODO
+    prob_loc_2 += pmf(loc_2_new, LOC_2_RETURN_LAMBDA) * sf(loc_2, LOC_2_RENTAL_LAMBDA) # TODO
 
     # We take the product of the two probabilities since each locations operates independently of the other.
     return prob_loc_1 * prob_loc_2
@@ -149,8 +149,19 @@ def expected_rewards(state: tuple[int], action: int, add_non_linearity: bool = T
     This is because we are caching this function and numpy arrays are not hashable!
     """
     # TODO
-    pass
-
+    loc_1_new = state[0] - action
+    loc_2_new = state[1] + action
+    r = (expectation(LOC_1_RENTAL_LAMBDA, loc_1_new) + expectation(LOC_2_RENTAL_LAMBDA, loc_2_new)) * RENTAL_REWARD
+    r -= abs(action) * MOVE_COST
+    if add_non_linearity:
+        if loc_1_new > STORAGE_CAPACITY:
+            r -= STORAGE_COST
+        if loc_2_new > STORAGE_CAPACITY:
+            r -= STORAGE_COST
+        r += MOVE_COST * max(min(action, FREE_MOVES), 0)
+        return r
+    else:
+        return r
 
 def q(state: np.ndarray, action: int, V: np.ndarray, add_non_linearity: bool) -> float:
     """
@@ -159,7 +170,11 @@ def q(state: np.ndarray, action: int, V: np.ndarray, add_non_linearity: bool) ->
     Implement this function and use it as a subroutine for policy evaluation and improvement.
     """
     # TODO
-    pass
+    S = [(i,j) for i in range(0, MAX_CARS_LOC_1+1) for j in range(0, MAX_CARS_LOC_2+1)]
+    tmp = 0
+    for s in S:
+        tmp += transition_prob(state, action, s) * V[s[0]][s[1]]
+    return tmp  
 
 def policy_evaluation(policy: np.ndarray, V: np.ndarray, add_non_linearity: bool, threshold: float):
     """
@@ -167,7 +182,16 @@ def policy_evaluation(policy: np.ndarray, V: np.ndarray, add_non_linearity: bool
     You should run the policy evaluation loop until the maximum change in `V` is less than `threshold`.
     """
     # TODO
-    pass
+    S = [(i,j) for i in range(0, MAX_CARS_LOC_1+1) for j in range(0, MAX_CARS_LOC_2+1)]
+    while True:
+        improve = 0
+        for s in S:
+            old_V = V[s[0]][s[1]]
+            a = policy[s[0]][s[1]]
+            V[s[0]][s[1]] = expected_rewards(s, a, add_non_linearity) + GAMMA * q(s, a, V, add_non_linearity)
+            improve = max(improve, abs(V[s[0]][s[1]] - old_V))
+        if improve < threshold:
+            break
 
 def policy_improvement(policy: np.ndarray, V: np.ndarray, add_non_linearity: bool, threshold: float):
     """
@@ -179,7 +203,28 @@ def policy_improvement(policy: np.ndarray, V: np.ndarray, add_non_linearity: boo
     For reference, the staff solution takes ~1.5 minutes to run.
     """
     # TODO
-    pass
+    # update V
+    policy_evaluation(policy, V, add_non_linearity, threshold)
+    # check policy optimality
+    policy_stable = True
+    S = [(i,j) for i in range(0, MAX_CARS_LOC_1+1) for j in range(0, MAX_CARS_LOC_2+1)]
+    for s in S:
+        old_policy = policy[s[0]][s[1]]
+        tmp_a = old_policy
+        tmp_v = V[s[0]][s[1]]
+        upper = min(s[0], MAX_CARS_MOVED, MAX_CARS_LOC_2-s[1]) 
+        lower = min(s[1], MAX_CARS_MOVED, MAX_CARS_LOC_1-s[0])
+        for a in range(-lower, upper+1):
+            value = expected_rewards(s, a, add_non_linearity) + GAMMA * q(s, a, V, add_non_linearity)
+            if value > tmp_v:
+                tmp_v = value
+                tmp_a = a
+        policy[s[0]][s[1]] = tmp_a
+        if policy_stable and (old_policy != policy[s[0]][s[1]]):
+            policy_stable = False
+    
+    if not policy_stable:
+        policy_improvement(policy, V, add_non_linearity, threshold)
 
 def value_iteration(V: np.ndarray, add_non_linearity: bool, threshold: float) -> np.ndarray:
     """
@@ -190,7 +235,23 @@ def value_iteration(V: np.ndarray, add_non_linearity: bool, threshold: float) ->
     For reference, the staff solution ~2 minutes to run.
     """
     # TODO
-    pass
+    S = [(i,j) for i in range(0, MAX_CARS_LOC_1+1) for j in range(0, MAX_CARS_LOC_2+1)]
+    policy = init_policy()
+    while True:
+        improve = 0
+        for s in S:
+            old_V = V[s[0]][s[1]]
+            upper = min(s[0], MAX_CARS_MOVED, MAX_CARS_LOC_2-s[1]) 
+            lower = min(s[1], MAX_CARS_MOVED, MAX_CARS_LOC_1-s[0])
+            for a in range(-lower, upper+1):
+                value = expected_rewards(s, a, add_non_linearity) + GAMMA * q(s, a, V, add_non_linearity)
+                if value > V[s[0]][s[1]]:
+                    V[s[0]][s[1]] = value
+                    policy[s[0]][s[1]] = a
+            improve = max(improve, abs(V[s[0]][s[1]] - old_V))
+        if improve < threshold:
+            break
+    return policy        
 
 def init_policy() -> np.ndarray:
     return np.zeros((MAX_CARS_LOC_1 + 1, MAX_CARS_LOC_2 + 1)).astype(np.int32)
